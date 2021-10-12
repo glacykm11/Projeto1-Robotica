22aab79f328ab937f2d5e323769f59b7cf0b4473
Gabrielly Moura <gabriellymouta@gmail.com>
2021-10-11 22:24:16 -0300

    updates

--- projeto1.py
+++ projeto1.py
@@ -8,52 +8,59 @@ if (clientID == 0):
   print('Conectado!')
 
 c = 0
-velocidade = 0.5 
+velocidade = 1
 
 #Motores 
 returnC, motorEsquerdo = vrep.simxGetObjectHandle(clientID,'Pioneer_p3dx_leftMotor',vrep.simx_opmode_oneshot_wait)
 returnC, motorDireito = vrep.simxGetObjectHandle(clientID,'Pioneer_p3dx_rightMotor',vrep.simx_opmode_oneshot_wait)
 
 #Sensores
-
+returnC, sensor3 = vrep.simxGetObjectHandle(clientID,'Pioneer_p3dx_ultrasonicSensor3',vrep.simx_opmode_oneshot_wait)
 returnC, sensor4 = vrep.simxGetObjectHandle(clientID,'Pioneer_p3dx_ultrasonicSensor4',vrep.simx_opmode_oneshot_wait)
 returnC, sensor5 = vrep.simxGetObjectHandle(clientID,'Pioneer_p3dx_ultrasonicSensor5',vrep.simx_opmode_oneshot_wait)
 returnC, sensor6 = vrep.simxGetObjectHandle(clientID,'Pioneer_p3dx_ultrasonicSensor6',vrep.simx_opmode_oneshot_wait)
-
-print("sensor 4:", sensor4)
 
 #Acesso à câmera
 returnC, camera = vrep.simxGetObjectHandle(clientID,'Vision_sensor',vrep.simx_opmode_oneshot_wait)
 
  #Leitura dos sensores como string
-returnC, detectionState, detectedPoint, detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(clientID,sensor4,vrep.simx_opmode_streaming)
-returnC, detectionState, detectedPoint, detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(clientID,sensor5,vrep.simx_opmode_streaming)
-returnC, detectionState, detectedPoint, detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(clientID,sensor6,vrep.simx_opmode_streaming)
-print("detectablePoint antes do loop:", detectedPoint)
+returnC, detectionState, detectedPoint3, detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(clientID,sensor3,vrep.simx_opmode_streaming)
+returnC, detectionState, detectedPoint4, detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(clientID,sensor4,vrep.simx_opmode_streaming)
+returnC, detectionState, detectedPoint5, detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(clientID,sensor5,vrep.simx_opmode_streaming)
+returnC, detectionState, detectedPoint6, detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(clientID,sensor6,vrep.simx_opmode_streaming)
+
 #Câmera
 returnC, resolution, image = vrep.simxGetVisionSensorImage(clientID,camera,1,vrep.simx_opmode_streaming)
 
 for i in range(200):
-    returnC, detectionState, detectedPoint,detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(clientID,sensor4,vrep.simx_opmode_buffer)
-    returnC, detectionState, detectedPoint,detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(clientID,sensor5,vrep.simx_opmode_buffer)
-    returnC, detectionState, detectedPoint,detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(clientID,sensor6,vrep.simx_opmode_buffer)
+    
+    returnC, detectionState, detectedPoint3, detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(clientID,sensor3,vrep.simx_opmode_buffer)
+    returnC, detectionState, detectedPoint4, detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(clientID,sensor4,vrep.simx_opmode_buffer)
+    returnC, detectionState, detectedPoint5, detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(clientID,sensor5,vrep.simx_opmode_buffer)
+    returnC, detectionState, detectedPoint6, detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(clientID,sensor6,vrep.simx_opmode_buffer)
 
-    # returnC,dist=vrep.simxReadProximitySensor(clientID,sensor4, vrep.simx_opmode_buffer);
-    print("detectablePoint depois do loop:", detectedPoint)
     returnC, resolution, image = vrep.simxGetVisionSensorImage(clientID,camera,1,vrep.simx_opmode_buffer)
-    a = np.linalg.norm(detectedPoint)
+    a3 = np.linalg.norm(detectedPoint3)
+    a4 = np.linalg.norm(detectedPoint4)
+    a5 = np.linalg.norm(detectedPoint5)
+    a6 = np.linalg.norm(detectedPoint6)
 
-    print("a: ", a)
-    if a > 0.01:
-        print('Avançar')
-        returnC = vrep.simxSetJointTargetVelocity(clientID,motorDireito,velocidade,vrep.simx_opmode_blocking)
+    print("a3: ", a3)
+    print("a4: ", a4)
+    print("a5: ", a5)
+    print("a6: ", a6)
+
+    if ((a3 >= 0.1) & (a3 < 0.75)) | ((a4 >= 0.1) & (a4 < 1)):
+        print('Girar Direita')
+        returnC = vrep.simxSetJointTargetVelocity(clientID,motorDireito,velocidade+1,vrep.simx_opmode_blocking)
         returnC = vrep.simxSetJointTargetVelocity(clientID,motorEsquerdo,velocidade,vrep.simx_opmode_blocking)
-    if (a > 0.01) & (a < 0.75):
-        print('Girar')
-        for j in range (10):
-            returnC = vrep.simxSetJointTargetVelocity(clientID,motorDireito,velocidade+0.75,vrep.simx_opmode_blocking)
-            returnC = vrep.simxSetJointTargetVelocity(clientID,motorEsquerdo,velocidade,vrep.simx_opmode_blocking)
+    if ((a5 >= 0.1) & (a5 < 1)) | ((a6 >= 0.1) & (a6 < 1)):
+        print('Girar Esquerda')
+        # for j in range (20):
+        returnC = vrep.simxSetJointTargetVelocity(clientID,motorDireito,velocidade, vrep.simx_opmode_blocking)
+        returnC = vrep.simxSetJointTargetVelocity(clientID,motorEsquerdo,velocidade+1,vrep.simx_opmode_blocking)
     else:
+        print('Avançar')
         returnC = vrep.simxSetJointTargetVelocity(clientID,motorDireito,velocidade,vrep.simx_opmode_blocking)
         returnC = vrep.simxSetJointTargetVelocity(clientID,motorEsquerdo,velocidade,vrep.simx_opmode_blocking)
 
